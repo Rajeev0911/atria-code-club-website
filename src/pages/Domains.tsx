8c6097fbb760483b7893aeb4dfdb4feec0e9e437
Rajeev <rajeevgid2023@gmail.com>
2024-01-28 17:00:12 +0530

    Readme updated

--- src/pages/Domains.tsx
+++ src/pages/Domains.tsx
@@ -4,28 +4,28 @@ import './Domains.css'
 const Domains = () => {
     return (
         <div className='card-collection'>
-            <div className="card" style={{width:'50%'}}>
+            <div className="card" style={{width:'70%'}}>
                 <div className="card-body">
                     <h5 className="card-title">AI and ML</h5>
                     <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae eius obcaecati praesentium sit reiciendis ducimus excepturi cum saepe. Enim, accusamus?</p>
                     <button className='domains-btn'>Select</button>
                 </div>
             </div>
-            <div className="card" style={{width:'50%'}}>
+            <div className="card" style={{width:'70%'}}>
                 <div className="card-body">
                     <h5 className="card-title">Website Development</h5>
                     <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nihil! Voluptas voluptatibus excepturi, minus reiciendis repudiandae impedit quisquam. Earum, doloremque!</p>
                     <button className='domains-btn'>Select</button>
                 </div>
             </div>
-            <div className="card" style={{width:'50%'}}>
+            <div className="card" style={{width:'70%'}}>
                 <div className="card-body">
                     <h5 className="card-title">DSA</h5>
                     <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A minima repellendus aspernatur reprehenderit dolor, fugiat beatae rerum animi ad iste.</p>
                     <button className='domains-btn'>Select</button>
                 </div>
             </div>
-            <div className="card" style={{width:'50%'}}>
+            <div className="card" style={{width:'70%'}}>
                 <div className="card-body">
                     <h5 className="card-title">Android develpoment</h5>
                     <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A blanditiis neque amet pariatur placeat cum saepe assumenda natus ipsum. At.</p>
